4c8824e65eb0ff9cc47b252ba8ce63f934524bac
Weisz Roland <weisz.roland@wevik.hu>
2021-09-02 21:04:23 +0200

    blank pygame screen ;-)

---
+++ code/main.py
@@ -0,0 +1,65 @@
+import pygame
+from pygame.locals import *
+from pygame import time
+from enum import Enum
+
+
+SCREEN_SIZE = (640, 480)
+FRAMERATE = 60
+BLACK = (0, 0, 0)
+
+
+State = Enum("State", "TITLE INSTRUCTIONS PLAY PAUSED OVER")
+
+
+class Main():
+    def __init__(self, screen_size:tuple, framerate:int, state:State, background:pygame.Color) -> None:
+        screen = pygame.display.set_mode(screen_size)
+        clock = time.Clock()
+        pygame.init()
+        self._run(screen, state, framerate, clock, background)
+    
+    def _run(self, screen:pygame.Surface, state:State, framerate:int, clock:time.Clock, background:pygame.Color) -> None:
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    state = State.OVER
+                if event.type == KEYDOWN:
+                    if event.key == K_SPACE:
+                        if state == State.TITLE:
+                            state = State.PLAY
+                        elif state == State.OVER:
+                            state = State.TITLE
+                    if event.key == K_ESCAPE:
+                        if state == State.TITLE:
+                            state = State.OVER
+                        elif state == State.INSTRUCTIONS:
+                            state = State.TITLE
+                        elif state == State.PLAY:
+                            state = State.PAUSED
+                        elif state == State.PAUSED:
+                            state = State.PLAY
+                        elif state == State.OVER:
+                            state = State.TITLE
+
+
+            screen.fill(background)
+
+            if state == State.TITLE:
+                pass
+            elif state == State.INSTRUCTIONS:
+                pass
+            elif state == State.PLAY:
+                pass
+            elif state == State.PAUSED:
+                pass
+            elif state == State.OVER:                
+                pygame.quit()
+                return
+
+            pygame.display.flip()
+            clock.tick(framerate)
+
+
+if __name__ == "__main__":
+    Main(SCREEN_SIZE, FRAMERATE, State.TITLE, BLACK)
